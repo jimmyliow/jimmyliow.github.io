3042ab306b837dd87d095e503fab7dd408e3bf9e
Jimmy Liow <lyj@hainatravel.com>
2024-07-16 15:15:03 +0800

    完成整体页面布局；解决 MD 样式

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,27 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jimmyliow.github.io',
   base: '/',
 	integrations: [mdx(), sitemap(), tailwind({applyBaseStyles: false})],
+  vite: {
+    plugins: [],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    optimizeDeps: {
+      allowNodeBuiltins: true
+    }
+  },
 });
